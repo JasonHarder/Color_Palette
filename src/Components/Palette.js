6893f5fbdfde898d95e22e9471ee13304fdee393
Anthematics <Jasonharder2@gmail.com>
2019-08-05 16:32:25 -0400

    fixed the background

--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -3,19 +3,21 @@ import Channel from "./Channel";
 import "../index.css";
 class ColorPalette extends React.Component {
   render() {
-    const swatchStyle = {backgroundColor: 'rgba(0,0,0,0)'}
+    const swatchStyle = { backgroundColor: 'rgba(0,0,0,alpha)'};
     return (
-      <div>
-        <ul className="palette">
-          <li className="swatch" style = {swatchStyle}>
-            <div> rgb (</div>
-            <Channel />
-            <Channel />
-            <Channel />
-            <div> ); </div>
-          </li>
-        </ul>
-      </div>
+      <main id="root" className = 'swatch' style = {swatchStyle}>
+        <div>
+          <ul className="palette">
+            <li className="swatch" style={swatchStyle}>
+              <div> rgb (</div>
+              <Channel />
+              <Channel />
+              <Channel />
+              <div> ); </div>
+            </li>
+          </ul>
+        </div>
+      </main>
     );
   }
 }
