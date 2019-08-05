dca087b4a17e22041c7dff2df4f0a1164ebf4b48
Anthematics <Jasonharder2@gmail.com>
2019-08-05 15:21:00 -0400

    fixed layout issue with color picker

--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import Channel from './Channel'
+import React from "react";
+import Channel from "./Channel";
+import "../index.css";
 class ColorPalette extends React.Component {
-  render () {
-    return(
+  render() {
+    const swatchStyle = {backgroundColor: 'rgba(0,0,0,0)'}
+    return (
       <div>
-      <Channel />
-      <Channel />
-      <Channel />
+        <ul className="palette">
+          <li className="swatch" style = {swatchStyle}>
+            <div> rgb (</div>
+            <Channel />
+            <Channel />
+            <Channel />
+            <div> ); </div>
+          </li>
+        </ul>
       </div>
-    )
+    );
   }
 }
 
-export default ColorPalette
+export default ColorPalette;
